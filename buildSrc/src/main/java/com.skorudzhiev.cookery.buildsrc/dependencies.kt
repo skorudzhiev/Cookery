d6a310adcfbc94ab7e954c9fc51402b350cda744
Stoyan Korudzhiev <stoyan.korudzhiev@sumup.com>
2021-04-16 22:39:57 +0300

    Move dependencies references to the buildsrc folder
    * Change the file to allow autocompletion when referenced

---
+++ buildSrc/src/main/java/com.skorudzhiev.cookery.buildsrc/dependencies.kt
@@ -0,0 +1,34 @@
+package com.skorudzhiev.cookery.buildsrc
+
+object Versions {
+    const val ktlint = "0.40.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha14"
+
+    object Kotlin {
+        private const val version = "1.4.32"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object AndroidX {
+        const val coreKtx = "androidx.core:core-ktx:1.6.0-alpha02"
+        const val appCompat = "androidx.appcompat:appcompat:1.2.0"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
+
+        object Test {
+            private const val version = "1.3.0"
+            const val core = "androidx.test:core:$version"
+            const val rules = "androidx.test:rules:$version"
+            const val junit = "androidx.test.ext:junit-ktx:1.1.2"
+            const val espresso = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+    }
+
+    object Google {
+        const val material = "com.google.android.material:material:1.3.0"
+    }
+}
