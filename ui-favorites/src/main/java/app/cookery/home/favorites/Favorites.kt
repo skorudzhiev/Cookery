1c8b3bdf78be34fab2b2e79d293e8993c98b898c
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2022-08-14 11:46:32 +0300

    [ui-favorites] Add placeholder when there are no Favorites stored

--- ui-favorites/src/main/java/app/cookery/home/favorites/Favorites.kt
+++ ui-favorites/src/main/java/app/cookery/home/favorites/Favorites.kt
@@ -1,23 +1,30 @@
 package app.cookery.home.favorites
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import app.cookery.data.entities.categories.CategoryDetails
 import com.cookery.common.compose.components.CategoryDetailsItem
 import com.cookery.common.compose.components.Header
 import com.cookery.common.compose.components.SnackBar
@@ -74,30 +81,65 @@ private fun Favorites(
                 modifier = Modifier.background(getAppBarColor()),
                 contentPadding = contentPadding
             ) {
-                item {
-                    Header(
-                        title = stringResource(R.string.favorites_screen_title),
-                        showAppBarBackground = false
-                    )
-                }
-
-                items(viewState.favorites) { meal ->
-                    Box(
-                        modifier = Modifier
-                            .clickable(onClick = { openMealDetails(meal.mealId) })
-                            .fillMaxSize()
-                            .padding(
-                                horizontal = Layout.bodyMargin,
-                                vertical = 2.dp
-                            )
-                    ) {
-                        CategoryDetailsItem(
-                            imageUrl = meal.mealImage,
-                            mealDescription = meal.mealName
+                if (viewState.favorites.isNotEmpty()) {
+                    item {
+                        Header(
+                            title = stringResource(R.string.favorites_screen_title),
+                            showAppBarBackground = false
                         )
+                    }
+
+                    items(viewState.favorites) { meal ->
+                        DisplayFavoriteMeals(
+                            categoryDetails = meal,
+                            openMealDetails = openMealDetails
+                        )
+                    }
+                } else {
+                    item {
+                        DisplayPlaceholder(Modifier.fillParentMaxSize())
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun DisplayFavoriteMeals(
+    categoryDetails: CategoryDetails,
+    openMealDetails: (String) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .clickable(onClick = { openMealDetails(categoryDetails.mealId) })
+            .fillMaxSize()
+            .padding(
+                horizontal = Layout.bodyMargin,
+                vertical = 2.dp
+            )
+    ) {
+        CategoryDetailsItem(
+            imageUrl = categoryDetails.mealImage,
+            mealDescription = categoryDetails.mealName
+        )
+    }
+}
+
+@Composable
+private fun DisplayPlaceholder(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_favorites_placeholder),
+            contentDescription = ""
+        )
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = stringResource(R.string.favorites_screen_placeholder)
+        )
     }
 }
